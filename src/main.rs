aa5c181db6e0aafa68f7854db5f288cf263108e2
shahidhk <shahidh@hasura.io>
2023-07-26 09:35:05 -0700

    add support for PORT env var

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::env;
 
 mod server;
 mod sql;
@@ -7,11 +8,12 @@ mod sql;
 async fn main() -> Result<(), Box<dyn Error>> {
     let router = server::router();
 
-    let adresss = "0.0.0.0:8080";
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("8080"));
+    let address = format!("0.0.0.0:{}", port).parse().expect("Failed to parse address and port");
 
-    println!("Starting server on {}", adresss);
+    println!("Starting server on {}", address);
 
-    axum::Server::bind(&adresss.parse()?)
+    axum::Server::bind(&address)
         .serve(router.into_make_service())
         .await?;
 
