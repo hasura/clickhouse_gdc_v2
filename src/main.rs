0ba32afbcf2a4da611a027b438ca262d2fe274b8
Benoit Ranque <ranque.benoit@gmail.com>
2023-07-26 13:59:10 -0400

    port configurable via env var or command line arg

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,25 @@ use std::error::Error;
 mod server;
 mod sql;
 
+use clap::Parser;
+
+#[derive(Parser)]
+struct ServerOptions {
+    #[arg(long, env, default_value_t = 8080)]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let router = server::router();
 
-    let adresss = "0.0.0.0:8080";
+    let options = ServerOptions::parse();
+
+    let adresss = format!("0.0.0.0:{}", options.port).parse()?;
 
     println!("Starting server on {}", adresss);
 
-    axum::Server::bind(&adresss.parse()?)
+    axum::Server::bind(&adresss)
         .serve(router.into_make_service())
         .await?;
 
