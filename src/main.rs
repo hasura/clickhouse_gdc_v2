89af7faa2753a819bdcfaccfb0e84a0c1a7495c3
Benoit Ranque <ranque.benoit@gmail.com>
2023-08-08 13:30:20 -0400

    tracing, graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::error::Error;
-
 mod server;
 mod sql;
 
 use clap::Parser;
+use std::{error::Error, net::SocketAddr};
 
 #[derive(Parser)]
 struct ServerOptions {
@@ -13,17 +12,47 @@ struct ServerOptions {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    init_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()?;
+
     let router = server::router();
 
     let options = ServerOptions::parse();
 
-    let adresss = format!("0.0.0.0:{}", options.port).parse()?;
+    let address: SocketAddr = format!("0.0.0.0:{}", options.port).parse()?;
 
-    println!("Starting server on {}", adresss);
+    tracing::info!("Server listening on port {}", address.port());
 
-    axum::Server::bind(&adresss)
+    axum::Server::bind(&address)
         .serve(router.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await?;
 
     Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::info!("Signal received, starting graceful shutdown");
+    opentelemetry::global::shutdown_tracer_provider();
 }
