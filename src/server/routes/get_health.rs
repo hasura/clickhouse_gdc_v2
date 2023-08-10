a2181987a75b9a55fde30871303901ce6ad6e109
Benoit Ranque <ranque.benoit@gmail.com>
2023-08-09 21:10:15 -0400

    make config optional for healthcheck

--- src/server/routes/get_health.rs
+++ src/server/routes/get_health.rs
@@ -4,8 +4,8 @@ use crate::server::config::{SourceConfig, SourceName};
 
 #[axum_macros::debug_handler]
 pub async fn get_health(
-    SourceName(_source_name): SourceName,
-    SourceConfig(_config): SourceConfig,
+    _source_name: Option<SourceName>,
+    _config: Option<SourceConfig>,
 ) -> StatusCode {
     // todo: if source_name and config provided, check if that specific source is healthy
     StatusCode::NO_CONTENT
