5091a51d930ae4e5d4673f3f3e4d43ae066eec47
Benoit Ranque <ranque.benoit@gmail.com>
2023-11-21 17:28:12 -0400

    filter tables in introspection result

--- src/server/routes/introspection/basic_info_unfiltered.sql
+++ src/server/routes/introspection/basic_info_unfiltered.sql
@@ -3,4 +3,5 @@ SELECT
     tables.table_type AS "table_type"
 FROM INFORMATION_SCHEMA.TABLES AS tables
 WHERE tables.table_catalog = currentDatabase()
+AND tables.table_type IN ('BASE TABLE', 'VIEW')
 FORMAT JSON;
